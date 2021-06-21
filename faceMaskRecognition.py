80749df75a242cc3f8310ed6735afa42142b6d53
Leonardo <leonardozecchin3@gmail.com>
2021-06-21 17:39:25 +0200

    Add some code but nothing of serious

--- faceMaskRecognition.py
+++ faceMaskRecognition.py
@@ -1,10 +1,35 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-img = cv2.imread('/media/leonardo/PopStorage/popOS/UNI/ML/FaceMaskRecognition/archive/Face Mask Dataset/Test/WithMask/53.png')
+imagesTrainWM_dir = './archive/FaceMaskDataset/Train/WithMask/'
+dirs = os.listdir(imagesTrainWM_dir)
+
+
+dim = (100,100)
+images = []
+for filename in os.listdir(imagesTrainWM_dir):
+    img = cv2.imread(os.path.join(imagesTrainWM_dir,filename))
+    if img is not None:
+        img = cv2.resize(img,dim,interpolation=cv2.INTER_AREA)
+        images.append(img)
+M = len(images)
+print(M)
+[r,c,ch] = images[0].shape
+
+print(images[0].shape)
+cv2.imshow('sample image',images[0])
+cv2.waitKey(0) # waits until a key is pressed
+cv2.destroyAllWindows() # destroys the window showing image
+
+
+
+
+
+'''img = cv2.imread('/media/leonardo/PopStorage/popOS/UNI/ML/FaceMaskRecognition/archive/Face Mask Dataset/Test/WithMask/53.png')
 
 cv2.imshow('sample image',img)
  
 cv2.waitKey(0) # waits until a key is pressed
-cv2.destroyAllWindows() # destroys the window showing image
+cv2.destroyAllWindows() # destroys the window showing image'''
