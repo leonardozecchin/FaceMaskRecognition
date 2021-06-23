7e5290d4753790fa01ed3cd16c342d439a95de55
fr@ncesco.localdomain <francesco.tubini@gmail.com>
2021-06-23 16:20:20 +0200

    Sorting eigenvalues of MA matrix and additional comment

--- faceMaskRecognition.m
+++ faceMaskRecognition.m
@@ -47,12 +47,12 @@ TMP = [TMP1,TMP2]; %Insieme di tutti i valori delle immagini
 %h1=figure; imshow(strcat(images_dirNM,'/',listNM(1).name)); title('Imshow'); 
 %set(gcf,'Name','Imshow','IntegerHandle','off'); 
 %colorbar
-%% LDA
+%% Prima parte LDA - Setup e procedura PCA
 TMP = double(TMP); %Casting a double
 media = mean(TMP,2); %Media del TMP
 AA(:,:) = TMP-repmat(media,1,M); %Sottrazione della media ai punti delle immagini
 [U,lambda] = eigen_training(AA); %Calcolo degli autovettori sulla matrice a cui è stata sottratta la media
-T = 200; % Numero dei migliori autovettori da tenere
+T = 200; % Numero dei migliori autovettori da tenere, questo implica anche la dimensione dei dati proiettati sul nuovo asse
 %Prima si fà PCA e poi LDA
 X = U(:,1:T)'*AA; %Proiezione dei punti senza media sugli autovettori migliori (QUESTA E' LA PROIZIONE DI PCA);
 l = reshape(repmat([1:2],5000,1),M,1); %Etichettatura delle prime 5000 immagini come immagini con mascherina e le seconde 5000 come senza mascherina
@@ -60,33 +60,35 @@ l = reshape(repmat([1:2],5000,1),M,1); %Etichettatura delle prime 5000 immagini 
 [d,N] = size(X); %Dimensione della matrice che contiene i punti proiettati seguendo PCA, quindi dimensione di X
 K = max(l); % numero classi in gioco;
 
+%% Seconda parte LDA - Calcolo matrici within e between class
+
 % 1. determino le classi Ck
 for k = 1:K
-    a = find (l == k);
-    Ck{k} = X(:,a);
+    a = find (l == k); %Prende le posizioni delle immagini prima che hanno etichetta 1 in l e poi 2
+    Ck{k} = X(:,a); %Salva nella struttura Ck (chiamata cella) tutte le X che appartengono prima alla classe 1 poi alla 2
 end
 
 % 2. determino le medie
 for k = 1:K
-    mk{k} = mean(Ck{k},2);
+    mk{k} = mean(Ck{k},2); %Media dei valori su 1 e media dei valori su 2
 end
 
 % 3. determino la numerosità della classe
 for k = 1:K
-    [d, Nk(k)] = size(Ck{k});
+    [d, Nk(k)] = size(Ck{k}); %Determino il numero di dati per ogni classe
 end
 
 % 4. determino le within class scatter
 for k = 1:K
-    S{k} = 0;
-    for i = 1:Nk(k)
-        S{k} = S{k} + (Ck{k}(:,i)-mk{k})*(Ck{k}(:,i)-mk{k})';
+    S{k} = 0; %Inizializza la cella a 0
+    for i = 1:Nk(k) %Nk è la size dei dati per ogni classe (numero di immagini (5000 per classe))
+        S{k} = S{k} + (Ck{k}(:,i)-mk{k})*(Ck{k}(:,i)-mk{k})'; %Calcola la matrice di Scatter ovvero la matrice di covarianza utilizzata per l'estrazione delle feature
     end
-    S{k} = S{k}./Nk(k);
+    S{k} = S{k}./Nk(k); %Divisione per il numero di immagini, credo serva per normalizzare la matrice di scatter
 end
 Swx = 0;
 for k = 1:K
-    Swx = Swx + S{k};
+    Swx = Swx + S{k}; %Within class scatter matrix
 end
 
 % 5. determino la between class covariance
@@ -94,35 +96,41 @@ end
 m = mean(X,2);
 Sbx = 0;
 for k=1:K
-    Sbx = Sbx + Nk(k)*((mk{k} - m)*(mk{k} - m)');
+    Sbx = Sbx + Nk(k)*((mk{k} - m)*(mk{k} - m)'); %Calcolo della between class scatter matrix
 end
-Sbx = Sbx/K;
+Sbx = Sbx/K; %Normalizzazione della between class scatter matrix sul numero di classi
 
-MA = inv(Swx)*Sbx;
+MA = inv(Swx)*Sbx; %Applicazione della LDA projection
+
+%% Terza parte LDA - Estrazione dell'autovettore e proiezione dei punti
 
 % eigenvalues/eigenvectors
-[V,D] = eig(MA);
+[V,D] = eig(MA); %Estazione degli autovettori dalla porecedente matrice
+
+%Sort degli auvalori della matrice MA
+D=diag(D);
+[D,ind]=sort(D,'descend');
+V = V(:,ind);
 
 % 5: transform matrix
-A = V(:,1);
+A = V(:,1); %Scelgo il singolo migliore autovettore su cui proiettare i punti
 
 % 6: transformation
-Y = A'*X;
+Y = A'*X; %Proiezione vera e propria
 
-%% PLOTTING
+%% Plotting dei dati proiettati
 
 % 7: plot
 figure, scatter(Y,ones(1,N),[],l)
-colormap jet
-for i=1:M
-    text(Y,num2str(l(i)))
-end
+%colormap jet
+%for i=1:M
+%    text(Y,num2str(l(i)))
+%end
 
-
-figure, scatter(TMP(1,:),TMP(2,:),[],l)
-for i=1:M
-    text(TMP(1,i),TMP(2,i),num2str(l(i)))
-end
+%figure, scatter(TMP(1,:),TMP(2,:),[],l)
+%for i=1:M
+%    text(TMP(1,i),TMP(2,i),num2str(l(i)))
+%end
 
 
 
